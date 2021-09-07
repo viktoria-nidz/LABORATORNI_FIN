aa1465f51ac01a2c930838b63b94726478cfa340
Viktoria <iraruslan1035@gmail.com>
2021-09-07 15:10:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORNI_FIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius:");
+            double radius;
+            radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter hight:");
+            double hight;
+            hight = Convert.ToDouble(Console.ReadLine());
+            double volume;
+            const double PI = 3.14;
+            volume = (Math.Pow(radius, 2) * hight * PI) * 1 / 3;
+            Console.WriteLine("the volume of the cone: " + volume);
+
+        }
+    }
+}
