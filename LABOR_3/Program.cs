cf27dd65c4ef2d6d59466498b20a0f0bfa1740bc
Viktoria <iraruslan1035@gmail.com>
2021-09-07 15:13:50 +0300

    LABOR_3

---
+++ LABOR_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABOR_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, x2, y2;
+            do
+            {
+                Console.WriteLine("Enter the koordinate x for the first field:");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the koordinate y for the first field:");
+                y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the koordinate x for the second field:");
+                x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the koordinate y for the second field:");
+                y2 = Convert.ToInt32(Console.ReadLine());
+            } while (x < 0 || x > 8 || x2 < 0 || x2 > 8 || y < 0 || y > 8 || y2 < 0 || y2 > 8);
+            bool check = true;
+            if (x == x2 || y == y2)
+            {
+                Console.WriteLine("TRUE");
+            }
+            else
+            {
+                Console.WriteLine("FALSE");
+            }
+
+        }
+    }
+}
