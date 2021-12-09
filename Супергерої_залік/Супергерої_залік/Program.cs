eb1a20fdd256f03a6283fea8f319b1b68ce5a2d8
Viktoriia Nidzelska <75253564+viktoria-nidz@users.noreply.github.com>
2021-12-09 10:09:54 +0200

    Add files via upload

---
+++ Супергерої_залік/Супергерої_залік/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Супергерої_залік
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Створити інтерфейс супергерой, та реалізувати 5 своїх супергероїв створивши їм окремі класи.
+        }
+    }
+}
